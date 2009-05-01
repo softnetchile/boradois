23459ec3bcfec94bb4e8a5983a214aca5b4e5186
iurifq <iurifq@8920f0e2-3653-11de-9d05-31133e6853b1>
2009-05-01 17:17:56 +0000

    * Added Hospede scaffold and presence validation.

---
+++ trunk/app/controllers/hospedes_controller.rb
@@ -0,0 +1,85 @@
+class HospedesController < ApplicationController
+  # GET /hospedes
+  # GET /hospedes.xml
+  def index
+    @hospedes = Hospede.find(:all)
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @hospedes }
+    end
+  end
+
+  # GET /hospedes/1
+  # GET /hospedes/1.xml
+  def show
+    @hospede = Hospede.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @hospede }
+    end
+  end
+
+  # GET /hospedes/new
+  # GET /hospedes/new.xml
+  def new
+    @hospede = Hospede.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @hospede }
+    end
+  end
+
+  # GET /hospedes/1/edit
+  def edit
+    @hospede = Hospede.find(params[:id])
+  end
+
+  # POST /hospedes
+  # POST /hospedes.xml
+  def create
+    @hospede = Hospede.new(params[:hospede])
+
+    respond_to do |format|
+      if @hospede.save
+        flash[:notice] = 'Hospede was successfully created.'
+        format.html { redirect_to(@hospede) }
+        format.xml  { render :xml => @hospede, :status => :created, :location => @hospede }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @hospede.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /hospedes/1
+  # PUT /hospedes/1.xml
+  def update
+    @hospede = Hospede.find(params[:id])
+
+    respond_to do |format|
+      if @hospede.update_attributes(params[:hospede])
+        flash[:notice] = 'Hospede was successfully updated.'
+        format.html { redirect_to(@hospede) }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @hospede.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /hospedes/1
+  # DELETE /hospedes/1.xml
+  def destroy
+    @hospede = Hospede.find(params[:id])
+    @hospede.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(hospedes_url) }
+      format.xml  { head :ok }
+    end
+  end
+end
---
+++ trunk/app/helpers/hospedes_helper.rb
@@ -0,0 +1,2 @@
+module HospedesHelper
+end
