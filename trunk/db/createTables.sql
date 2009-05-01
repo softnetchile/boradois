7a64e46d69eba894ce2269205d60e1ce7bbe5ec8
iurifq <iurifq@8920f0e2-3653-11de-9d05-31133e6853b1>
2009-05-01 18:01:47 +0000

    * Added Servico scaffold and some validation. * Added tables servicos, apartamentos and recepcionistas db/createTables.sql.

--- trunk/db/createTables.sql
+++ trunk/db/createTables.sql
@@ -10,3 +10,37 @@ create table hospedes (
 	estCivil	ENUM('Casado', 'Solteiro', 'Divorciado', 'Viuvo')	not null,
 	primary key(id)
 );
+
+drop table if exists recepcionistas;
+
+create table recepcionistas (
+	id			int				not	null auto_increment,
+	nome 		varchar(100)	not	null,
+	endereco	varchar(300)	not null,
+	nascimento	date			not null,
+	sexo		ENUM('Masculino', 'Feminino')	not null,
+	estCivil	ENUM('Casado', 'Solteiro', 'Divorciado', 'Viuvo')	not null,
+	login		varchar(30)		not null,
+	password	varchar(50)		not null,
+	primary key(id)
+);
+
+drop table if exists apartamentos;
+
+create table apartamentos (
+	id			int				not	null auto_increment,
+	num			int				not	null unique,
+	tipo		ENUM('Executivo', 'Luxo', 'Presidencial')	not null,
+	livre		boolean			default true,
+	primary key(id)
+);
+
+drop table if exists servicos;
+
+create table servicos (
+	id			int				not	null auto_increment,
+	nome 		varchar(100)	not	null,
+	valor		float			not null,
+	descricao	text			not null,
+	primary key(id)
+);
