61948bf45c4826ba6480de4724af4e9661d9f81c
iurifq <iurifq@8920f0e2-3653-11de-9d05-31133e6853b1>
2009-05-02 19:17:31 +0000

    * Moved possible 'generos' and 'estados civis" from views to controllers.

--- trunk/app/models/hospede.rb
+++ trunk/app/models/hospede.rb
@@ -2,4 +2,12 @@ class Hospede < ActiveRecord::Base
 
 	validates_presence_of :nome, :endereco, :nascimento, :pais, :sexo, :estCivil
 
+	def self.estadosCivis
+		%w[Casado Solteiro Divorciado Viuvo]
+	end
+
+	def self.generos
+		%w[Masculino Feminino]
+	end
+
 end
