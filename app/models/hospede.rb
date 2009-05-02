3d978d36526d864266a6ece890dc2e71133ffff2
suzana.matos <suzana.matos@8920f0e2-3653-11de-9d05-31133e6853b1>
2009-05-02 18:02:42 +0000

    Comboboxes

--- app/models/hospede.rb
+++ app/models/hospede.rb
@@ -1,9 +1,5 @@
 class Hospede < ActiveRecord::Base
 
 	validates_presence_of :nome, :endereco, :nascimento, :pais, :sexo, :estCivil
-	SEXO_OPCOES = [
-		[ "Masculino", "m" ],
-		[ "Feminino", "f"]
-	].freeze
 
 end
