d8b72fed9bd05e1b83019bd79b009372ba188bf8
iurifq <iurifq@8920f0e2-3653-11de-9d05-31133e6853b1>
2009-05-04 22:37:09 +0000

    * Created  table reservas, but the foreign key for hospedes is not working.

--- trunk/db/createTables.sql
+++ trunk/db/createTables.sql
@@ -1,16 +1,3 @@
-drop table if exists hospedes;
-
-create table hospedes (
-	id			int				not	null auto_increment,
-	nome 		varchar(100)	not	null,
-	endereco	varchar(300)	not null,
-	nascimento	date			not null,
-	pais		varchar(50)		not null,
-	sexo		ENUM('Masculino', 'Feminino')	not null,
-	estCivil	ENUM('Casado', 'Solteiro', 'Divorciado', 'Viuvo')	not null,
-	primary key(id)
-);
-
 drop table if exists recepcionistas;
 
 create table recepcionistas (
@@ -22,6 +9,21 @@ create table recepcionistas (
 	estCivil	ENUM('Casado', 'Solteiro', 'Divorciado', 'Viuvo')	not null,
 	login		varchar(30)		not null,
 	password	varchar(50)		not null,
+	primary key(id)
+);
+
+drop table if exists reservas;
+
+drop table if exists hospedes;
+
+create table hospedes (
+	id			int				not	null auto_increment,
+	nome 		varchar(100)	not	null,
+	endereco	varchar(300)	not null,
+	nascimento	date			not null,
+	pais		varchar(50)		not null,
+	sexo		ENUM('Masculino', 'Feminino')	not null,
+	estCivil	ENUM('Casado', 'Solteiro', 'Divorciado', 'Viuvo')	not null,
 	primary key(id)
 );
 
@@ -37,11 +39,23 @@ create table tipos_apartamentos (
 	primary key(id)
 ) ENGINE = InnoDB;
 
-create table apartamentos (
-	id			int				not	null auto_increment,
-	num			int				not	null unique,
+create table reservas (
+	id			int		not null auto_increment,
 	fkTiposApartamentos	int		not null,
-	tipo		ENUM('Executivo', 'Luxo', 'Presidencial')	not null,
+	fkHospedes		int		not null,
+	dataEntrada		Date,
+	dataSaida		Date,
+	primary key(id),
+	index (fkTiposApartamentos),
+	index (fkHospedes),
+	foreign key (fkTiposApartamentos)	references tiposApartamentos(id)	on delete cascade on update cascade,
+	foreign key (fkHospedes)			references hospedes(id)				on delete cascade on update cascade
+) ENGINE = InnoDB;
+
+create table apartamentos (
+	id			int		not null auto_increment,
+	num			int		not null unique,
+	fkTiposApartamentos	int		not null,
 	livre		boolean			default true,
 	primary key(id),
 	index (fkTiposApartamentos),
@@ -52,8 +66,8 @@ create table apartamentos (
 drop table if exists servicos;
 
 create table servicos (
-	id			int				not	null auto_increment,
-	nome 		varchar(100)	not	null,
+	id			int		not null auto_increment,
+	nome 		varchar(100)		not null,
 	valor		float			not null,
 	descricao	text			not null,
 	primary key(id)
