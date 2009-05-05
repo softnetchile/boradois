98965814db3e6b3a632bece3f50a343f43650d46
suzana.matos <suzana.matos@8920f0e2-3653-11de-9d05-31133e6853b1>
2009-05-05 05:13:49 +0000

    Dates and labels

--- app/models/apartamento.rb
+++ app/models/apartamento.rb
@@ -1,3 +1,7 @@
 class Apartamento < ActiveRecord::Base
 	belongs_to :tipos_apartamentos
+	
+	validates_presence_of :num, :fkTiposApartamentos
+	validates_uniqueness_of :num
+
 end
--- app/models/reserva.rb
+++ app/models/reserva.rb
@@ -2,15 +2,19 @@ class Reserva < ActiveRecord::Base
 	belongs_to :tipos_apartamentos
 	belongs_to :hospedes
 
-	def self.okToReserve(dataEntrada,dataSaida,fkTipoApartamento)
-		if dataEntrada <= dataSaida
-			@totalAp=Apartamento.count(:conditions => ["fkTiposApartamentos = ?",fkTipoApartamento)
-			@ocupados=Reserva.count(:conditions => ["fkTiposApartamentos = ? AND (( dataSaida < ? AND dataSaida > ? ) OR (dataEntrada < ? AND dataEntrada > ? ))", fkTipoApartamento, dataSaida, dataEntrada, dataSaida, dataEntrada)
+	def self.okToReserve(dataEntrada, dataSaida, fkTiposApartamentos)
+		if dataEntrada <= dataSaida then
+			@totalAp=Apartamento.count(:conditions => ["fkTiposApartamentos = ?",fkTiposApartamentos])
+			@ocupados=Reserva.count(:conditions => ["fkTiposApartamentos = ? AND (( dataSaida < ? AND dataSaida > ? ) OR (dataEntrada < ? AND dataEntrada > ? ))", fkTiposApartamentos, dataSaida, dataEntrada, dataSaida, dataEntrada])
 			if @ocupados < @totalAp
 				return true
 			end
 		end
 		return false
 	end
-
+	
+	def validate
+		errors.add( "Reserva não pode ser feita") unless Reserva.okToReserve :dataEntrada, :dataSaida, :fkTiposApartamentos
+		#não sei se isso tah ok sem o primeiro argumento... mas passou
+	end
 end
