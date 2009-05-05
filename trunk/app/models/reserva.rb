bccb049a001b38130d6e536d15feec3b1c01e5d8
iurifq <iurifq@8920f0e2-3653-11de-9d05-31133e6853b1>
2009-05-05 04:08:55 +0000

    * Made validation method okToReserve to be used to validade if there's

--- trunk/app/models/reserva.rb
+++ trunk/app/models/reserva.rb
@@ -1,4 +1,16 @@
 class Reserva < ActiveRecord::Base
 	belongs_to :tipos_apartamentos
 	belongs_to :hospedes
+
+	def self.okToReserve(dataEntrada,dataSaida,fkTipoApartamento)
+		if dataEntrada <= dataSaida
+			@totalAp=Apartamento.count(:conditions => ["fkTiposApartamentos = ?",fkTipoApartamento)
+			@ocupados=Reserva.count(:conditions => ["fkTiposApartamentos = ? AND (( dataSaida < ? AND dataSaida > ? ) OR (dataEntrada < ? AND dataEntrada > ? ))", fkTipoApartamento, dataSaida, dataEntrada, dataSaida, dataEntrada)
+			if @ocupados < @totalAp
+				return true
+			end
+		end
+		return false
+	end
+
 end
