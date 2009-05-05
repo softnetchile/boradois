4edc681c09d36f8c14a44ea134c7b5b1858d244e
iurifq <iurifq@8920f0e2-3653-11de-9d05-31133e6853b1>
2009-05-05 11:50:14 +0000

    * Created missing validations.

--- trunk/app/models/apartamento.rb
+++ trunk/app/models/apartamento.rb
@@ -3,5 +3,10 @@ class Apartamento < ActiveRecord::Base
 	
 	validates_presence_of :num, :fkTiposApartamentos
 	validates_uniqueness_of :num
+	validates_numericality_of :num
 
+	protected
+	def validate
+		errors.add(:num, "deve ser positivo") unless num.nil? || num > 0.0
+	end
 end
