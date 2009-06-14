cf06f62e085afd1a9870b0c149510370ddb248fe
iurifq <iurifq@8920f0e2-3653-11de-9d05-31133e6853b1>
2009-06-14 18:27:49 +0000

    * "Consulta 1" done * Created link in admin_menu

--- trunk/app/controllers/consultas_controller.rb
+++ trunk/app/controllers/consultas_controller.rb
@@ -9,8 +9,27 @@ class ConsultasController < ApplicationController
     end
   end
 
+  def con1
+	if request.post?
+		data = Date.civil(params[:post][:"data(1i)"].to_i,params[:post][:"data(2i)"].to_i,params[:post][:"data(3i)"].to_i)
+		if data <= Date.today
+			total = Apartamento.find(:all).size
+			ocupados = Conta.find(	:all,
+									:conditions => ["(dataEntr <= ? and dataSaid IS NULL) or (dataEntr <= ? and dataSaid >= ?)",data.beginning_of_day,data.beginning_of_day,data.end_of_day],
+									:joins => "as b inner join contas_apartamentos as a on a.conta_id = b.id",
+									:select => "a.apartamento_id").size
+			@taxa_ocupacao = Float(ocupados)/total
+		else
+			flash[:notice] = "A data escolhida deve anterior a data atual"
+		end
+	end
+    respond_to do |format|
+      format.html # con1.html.erb
+      format.xml  { render :xml }
+    end
+  end
+
   def con4
-	month = Time.now.month
 	@totais = Aluguel.find(	:all,
 							:conditions => ["dataHora >= ?", Time.now.beginning_of_month ],
 							:joins => "as b inner join servicos as a on b.servico_id = a.id ",
